ee48a7c21fa3410931085d3db6ecb7103e01fc16
bessax <andrebessax@gmail.com>
2024-10-07 09:55:55 -0300

    #update: ajustes.

--- dotnet_async/Program.cs
+++ dotnet_async/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿void LerArquivo()
+{
+    var conteudo = File.ReadAllText("voos.txt");
+    Console.WriteLine($"Conteúdo: \n{conteudo}");
+}
+LerArquivo();
+Console.WriteLine("Outras operações.");
+Console.ReadKey();
