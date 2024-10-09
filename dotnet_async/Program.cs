1b77722d611e03c2fd7e935d854b435b9968b95d
bessax <andrebessax@gmail.com>
2024-10-09 10:48:24 -0300

    #Update: vídeo 4.3

--- dotnet_async/Program.cs
+++ dotnet_async/Program.cs
@@ -1,50 +1,68 @@
-﻿object chave = new object();
-Task<string> conteudoTask;
-lock(chave)
+﻿#region Programação Assíncrona
+//object chave = new object();
+//Task<string> conteudoTask;
+//lock(chave)
+//{
+//    conteudoTask = Task.Run(() => File.ReadAllTextAsync("voos.txt"));
+//}
+//async Task LerArquivoAsync(CancellationToken token)
+//{
+//    try
+//    {
+//        await Task.Delay(new Random().Next(300, 8000));
+//        token.ThrowIfCancellationRequested();
+//        Console.WriteLine($"Conteúdo: \n{conteudoTask.Result}");
+
+//    }
+//    catch (OperationCanceledException ex)
+//    {
+//       Console.WriteLine($"Tarefa cancelada: {ex.Message}");
+//    }
+//    catch (AggregateException ex)
+//    {
+//        Console.WriteLine($"Aconteceu o erro: {ex.InnerException.Message}");
+//    }
+
+//}
+
+//async Task ExibirRelatorioAsync(CancellationToken token)
+//{
+//	try
+//	{
+//        Console.WriteLine("Executando relatório de compra de passagens!");
+//        await Task.Delay(new Random().Next(300, 8000));
+//        token.ThrowIfCancellationRequested();
+//    }
+//	catch(OperationCanceledException ex)
+//	{
+
+//        Console.WriteLine($"Tarefa cancelada: {ex.Message}");
+//	}
+
+//}
+
+//CancellationTokenSource tokenDeCancelamento = new CancellationTokenSource();
+
+//Task tarefa= Task.WhenAll(LerArquivoAsync(tokenDeCancelamento.Token), ExibirRelatorioAsync(tokenDeCancelamento.Token));
+
+//await Task.Delay(1000).ContinueWith(_ => tokenDeCancelamento.Cancel());
+
+//Console.WriteLine("Outras operações.");
+//Console.ReadKey();
+#endregion
+
+using dotnet_async.Client;
+
+var client = new JornadaMilhasClient(new JornadaMilhasClientFactory().CreateClient());
+
+async Task ProcessarConsultasDeVoosAsync()
 {
-    conteudoTask = Task.Run(() => File.ReadAllTextAsync("voos.txt"));
-}
-async Task LerArquivoAsync(CancellationToken token)
-{
-    try
+    var voos = await client.ConsultarVoos();
+
+    foreach (var voo in voos)
     {
-        await Task.Delay(new Random().Next(300, 8000));
-        token.ThrowIfCancellationRequested();
-        Console.WriteLine($"Conteúdo: \n{conteudoTask.Result}");
-        
+        Console.WriteLine(voo);
     }
-    catch (OperationCanceledException ex)
-    {
-       Console.WriteLine($"Tarefa cancelada: {ex.Message}");
-    }
-    catch (AggregateException ex)
-    {
-        Console.WriteLine($"Aconteceu o erro: {ex.InnerException.Message}");
-    }
-    
 }
 
-async Task ExibirRelatorioAsync(CancellationToken token)
-{
-	try
-	{
-        Console.WriteLine("Executando relatório de compra de passagens!");
-        await Task.Delay(new Random().Next(300, 8000));
-        token.ThrowIfCancellationRequested();
-    }
-	catch(OperationCanceledException ex)
-	{
-
-        Console.WriteLine($"Tarefa cancelada: {ex.Message}");
-	}
-    
-}
-
-CancellationTokenSource tokenDeCancelamento = new CancellationTokenSource();
-
-Task tarefa= Task.WhenAll(LerArquivoAsync(tokenDeCancelamento.Token), ExibirRelatorioAsync(tokenDeCancelamento.Token));
-
-await Task.Delay(1000).ContinueWith(_ => tokenDeCancelamento.Cancel());
-
-Console.WriteLine("Outras operações.");
-Console.ReadKey();
+await ProcessarConsultasDeVoosAsync();
